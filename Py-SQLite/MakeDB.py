0a29294cead6c1da0efa330eee983a7751500a8a
aKuad <tw: @a_kuad>
2021-10-21 01:17:25 +0900

    Time measurement installed in MakeDB.py

--- Py-SQLite/MakeDB.py
+++ Py-SQLite/MakeDB.py
@@ -10,29 +10,40 @@
 # Modules importing
 from sqlite3 import connect
 from subprocess import call
+from time import perf_counter
 
 
 # Parameters settings
 NAME_DB = "data.db"
 NAME_TB = "data"
-NAME_TEXT = "../data.txt"
+NAME_TX = "../data.txt"
 
 
 # Database making
 ## Database connect
+call("echo -n > %s" % NAME_DB, shell=True)  # Exist file removing
 conn = connect(NAME_DB)
 curs = conn.cursor()
 
+## Timer start
+t_st = perf_counter()
+
 ## Table make
-call("echo -n > %s" % NAME_DB, shell=True)
 curs.execute("create table %s (id int, value char(6));" % NAME_TB)
 
 ## Data inserting
-with open(NAME_TEXT, "r") as f:
+with open(NAME_TX, "r") as f:
   for line in f.readlines():
     data = line.strip().split(" ")
     curs.execute("insert into %s values (%s, '%s');" % (NAME_TB, data[0], data[1]))
 
+## Timer stop
+t_ed = perf_counter()
+
 ## Database disconnect
 conn.commit()
 conn.close()
+
+
+# Process time printing
+print("%f" % ((t_ed - t_st) * 1000))
