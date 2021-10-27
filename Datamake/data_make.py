4f5596a31afa310f38982089615b3183bc15fea3
aKuad <tw:@a_kuad>
2021-10-27 09:59:58 +0900

    Parameter specifying changed constants to arguments

--- Datamake/data_make.py
+++ Datamake/data_make.py
@@ -9,10 +9,20 @@
 
 # Modules importing
 from random import randint
+from sys import argv, stderr, exit
+
+
+# Arguments checking
+if len(argv) != 3:
+  print("Invalid arguments.", file=stderr)
+  print("  Useage:", file=stderr)
+  print("    %s <Number of data> <Output file name>" % argv[0], file=stderr)
+  exit(1)
+
 
 # Parameters settings
-DATA_COUNT = 1000
-DATA_NAME = "../data.txt"
+DATA_COUNT = int(argv[1])
+DATA_NAME = argv[2]
 
 # Data making and writing
 with open(DATA_NAME, 'w') as f:
